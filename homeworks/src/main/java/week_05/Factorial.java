32a7c1cd2eb65df79b617c71c5cdf2ab0c0b4464
Adela-cloud <dilov222@gmail.com>
2024-11-14 14:12:04 +0900

    some algo questions

--- homeworks/src/main/java/week_05/Factorial.java
+++ homeworks/src/main/java/week_05/Factorial.java
@@ -1,5 +1,7 @@
 package week_05;
 
+import java.util.IllegalFormatCodePointException;
+
 /**
  * factorial mark = !
  * 0!= 1
@@ -7,4 +9,35 @@ package week_05;
  */
 
 public class Factorial {
+
+    //time complexity: O(n)
+    //space: O(1)
+    public static int factorial(int num){
+      if (num == 0)
+          return 1;
+
+      int fact= 1;
+      for (int i = 1;  i<= num; i++ ){
+          fact *=  i;
+      }
+      return fact;
+    }
+
+
+    public static long factorial1(int n) {
+        if(n==0){
+            return 1L;
+        }
+        int fact= 1;
+        for(int i= 1; i<= n; i++){
+            fact*= i;
+        }
+        return Long.valueOf(fact);
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(factorial(4));
+        System.out.println(factorial1(4));
+    }
 }
