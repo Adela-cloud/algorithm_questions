360e07d59141a4ee52a4409c3c4057d6a4752e1e
Adela-cloud <dilov222@gmail.com>
2024-11-15 13:29:57 +0900

    remove Vowels and spaces

--- homeworks/src/main/java/week_05/VowelRemover.java
+++ homeworks/src/main/java/week_05/VowelRemover.java
@@ -1,4 +1,6 @@
 package week_05;
+
+
 /**
  * Remove Vowels and Spaces from a String
  * Given a string str, write a method that removes the vowels 'a', 'e', 'i', 'o', 'u' and spaces from it, and returns the new string.
@@ -21,4 +23,32 @@ package week_05;
  */
 
 public class VowelRemover {
+
+    //solution 1 : With StringBuilder
+    //time Complexity : O(n)
+    //space complexity: O(n)
+    public static String removeVowelsAndSpaces(String text){
+       StringBuilder   result = new StringBuilder();
+       for (char ch: text.toCharArray()){
+           if (ch != 'a' && ch != 'e'&& ch != 'u' && ch != 'i' && ch != 'o' && ch != ' '){
+               result.append(ch);
+           }
+        }
+       return result.toString();
+    }
+
+
+    // solution 2 : with regex
+    public static String removeVowelsWithRegex(String text){
+       return text.replaceAll("[aeiou\\s]", "");
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(removeVowelsAndSpaces("everything is an object"));
+        System.out.println(removeVowelsAndSpaces("ae iou"));
+
+        System.out.println(removeVowelsWithRegex("everything is an object"));
+        System.out.println(removeVowelsWithRegex("ae iou"));
+    }
 }
